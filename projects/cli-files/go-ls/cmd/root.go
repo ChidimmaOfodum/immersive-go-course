2872d247d7007d7587d8dc0302e895ebe2994850
Chidimma <chidimmamofodum@gmail.com>
2024-04-23 09:52:07 -0400

    implemeniting help flag

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"flag"
 	"strings"
 )
 func Execute() {
-	args := os.Args[1:]
+	help := flag.Bool("help", false, "describes how to use go-ls command")
+    flag.Parse()
+
+    if *help {
+        flag.PrintDefaults()
+        fmt.Println("Usage: go-ls [directories]")
+        return
+    }
+	//args := os.Args[1:]
+	args := flag.Args()
 	
 	if len(args) == 0 {
 		args = []string{"."}

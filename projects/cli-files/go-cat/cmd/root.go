64d6b94ac6bc4e5c4f2f3c62530165cd0caf63ff
Chidimma <chidimmamofodum@gmail.com>
2024-05-14 09:51:02 -0400

    scanner to read date one at a time instead of readfile

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -4,20 +4,33 @@ import (
 	"os"
 	"bufio"
 	"fmt"
+	"flag"
 )
 
 func Execute() (error) {
-	args := os.Args[1:]
+	lineNum := flag.Bool("n", false, "Number the output lines, starting at 1")
+	flag.Parse()
+
+	args := flag.Args()
 	for _, fileName := range args {
 		file, err := os.Open(fileName)
 		if err != nil {
 			return err
 		}
 		scanner := bufio.NewScanner(file)
-
-		for scanner.Scan() {
-			fmt.Printf("%v\n", scanner.Text())	
-		}
+		printFileContent(*lineNum, scanner)
 	}
 	return nil
+}
+
+func printFileContent(displayLineNum bool, scanner *bufio.Scanner) (int) {
+	lineNumber := 0
+	for scanner.Scan() {
+		lineNumber++
+		if displayLineNum {
+			fmt.Printf("%d  ", lineNumber)
+		}
+		fmt.Printf("%v\n", scanner.Text())
+	}
+	return lineNumber
 }

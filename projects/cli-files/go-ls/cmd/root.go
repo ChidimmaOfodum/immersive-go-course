9cdaafe49062c0382b5d3a0fa363e383a30d738c
Chidimma <chidimmamofodum@gmail.com>
2024-05-17 08:32:18 -0400

    implementing -m flag

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -11,42 +11,54 @@ func Execute()(error) {
 	var fileInfo fs.FileInfo
 	var err error
 
+	//flags
 	help := flag.Bool("h", false, "describes how to use go-ls command")
+	comma:= flag.Bool("m", false, "prints with a delimiter")
 	flag.Parse()
+
+	delimiter := "\t"
 
 	if *help {
 		flag.PrintDefaults()
 		fmt.Println("Usage: go-ls [directories]")
 		return nil
 	}
+	if *comma {
+		delimiter = ","
+	}
+
 	args := flag.Args()
 
 	if len(args) == 0 {
 		args = []string{"."}
 	}
 
-	for _, y := range args {
+	for _, filePath := range args {
 
-		fileInfo, err = os.Stat(y)
+		fileInfo, err = os.Stat(filePath)
 
 		if err != nil {
 			return err
 		}
 		if fileInfo.IsDir() {
-			if err := listFiles(y); err != nil {
+			files, err := os.ReadDir(filePath)
+
+			if err != nil {
 				return err
 			}
+			listFiles(files, delimiter)
 		} else {
-			fmt.Printf("%s\n", fileInfo.Name())
+			fmt.Printf("%s\n", fileInfo.Name()) // print filename if not directory
 		}
 	}
 	return nil
 }
 
-func listFiles(name string) (error) {
-	files, err := os.ReadDir(name)
-	for _, v := range files {
-		fmt.Printf("%s\n", v.Name())
+func listFiles(fileNames []fs.DirEntry, delim string) {
+	for index, value := range fileNames {
+		fmt.Printf("%s", value.Name())
+		if index != len(fileNames) - 1 {
+			fmt.Printf("%s ", delim)
+		}
 	}
-	return err
 }

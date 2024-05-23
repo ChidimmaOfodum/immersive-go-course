c2641c4eab41462edc00da6f635ee814c7371797
Chidimma <chidimmamofodum@gmail.com>
2024-05-23 10:42:10 -0400

    documenting limitations for ourbytebuffer

--- projects/interfaces/buffer.go
+++ projects/interfaces/buffer.go
@@ -5,6 +5,10 @@ import (
 	"errors"
 )
 
+//OurByteBuffer - Limitations
+// - the buffer grows and shrinks as data is added and read from it. The maximum data stored by the data is dependent on the machine in which the code is run. The buffer frees up all memory that has been successfully read from the buffer.
+//Both read and write operations modifies the buffer, hence it is unsafe to perform them concurrently. Retrieving bytes is also unsafe to run concurrently with either read or write operations.
+
 type OurByteBuffer struct {
 	buffer []byte
 }

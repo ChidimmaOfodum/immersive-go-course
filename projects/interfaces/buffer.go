35b567290c5191f88022619bd97fff77cfe27191
Chidimma <chidimmamofodum@gmail.com>
2024-06-16 10:06:07 -0400

    applying review suggestions

--- projects/interfaces/buffer.go
+++ projects/interfaces/buffer.go
@@ -1,12 +1,11 @@
 package interfaces
 
 import (
-	"slices"
-	"errors"
+	"io"
 )
 
 //OurByteBuffer - Limitations
-// - the buffer grows and shrinks as data is added and read from it. The maximum data stored by the data is dependent on the machine in which the code is run. The buffer frees up all memory that has been successfully read from the buffer.
+// - the buffer grows and shrinks as data is added and read from it. The maximum data stored by the buffer is dependent on the machine in which the code is run.
 //Both read and write operations modifies the buffer, hence it is unsafe to perform them concurrently. Retrieving bytes is also unsafe to run concurrently with either read or write operations.
 
 type OurByteBuffer struct {
@@ -15,28 +14,21 @@ type OurByteBuffer struct {
 
 
 func (b *OurByteBuffer) Write(data []byte) (int, error) {
-	count := 0
-	for _, v := range data {
-		result := append(b.buffer, v)
-		b.buffer = result
-		count++
-	}
-	return count, nil
+	b.buffer = append(b.buffer, data...)
+	return len(data), nil
 }
 
 func (b *OurByteBuffer) Bytes() []byte {
 	return b.buffer
 }
 
+//Read returns io.EOF error (unless len(dest) is zero) when buffer has no data
 func (b *OurByteBuffer) Read(dest []byte) (int, error) {
 	var err error
-	if b.buffer == nil && len(dest) != 0 {
-		err = errors.New("EOF")
+	if len(b.buffer) == 0 && len(dest) != 0 {
+		err = io.EOF
 	}
 	byteCopied := copy(dest, b.buffer)
-	newBuffer := slices.Delete(b.buffer, 0, byteCopied)
-	b.buffer = newBuffer
+	b.buffer = b.buffer[byteCopied:]
 	return byteCopied, err
 }
-
-

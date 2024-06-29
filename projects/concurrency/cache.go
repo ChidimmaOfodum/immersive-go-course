3d06b8bb6def6ca67067dbd0aacf6beee5a9f5b5
Chidimma <chidimmamofodum@gmail.com>
2024-06-29 12:45:14 -0400

    declaring statistics and cacheEntry struct

--- projects/concurrency/cache.go
+++ projects/concurrency/cache.go
@@ -1,24 +1,49 @@
 package concurrency
 
+import (
+	"time"
+)
+
+type Statistics struct {
+	hitRate        int
+	unReadEndtries int
+	averageHit     float32
+	readsAndWrite  int
+}
+
+type cacheEntry[V any]struct {
+	value V
+	readTime time.Time
+}
+
 type Cache[K comparable, V any] struct {
 	entryLimit int
-	cache      map[K]V
+	cache      map[K]cacheEntry[V]
+	statistics Statistics
 }
 
 func NewCache[K comparable, V any](entryLimit int) Cache[K, V] {
-	return Cache[K, V]{entryLimit: entryLimit, cache: make(map[K]V, entryLimit)}
+	return Cache[K, V]{entryLimit: entryLimit, cache: make(map[K]cacheEntry[V])}
 }
 
 func (c *Cache[K, V]) Put(key K, value V) bool {
 	_, ok := c.cache[key]
-	c.cache[key] = value
+
+	c.cache[key] = struct {
+		value    V
+		readTime time.Time
+	}{value: value, readTime: time.Time{}}
 	return ok
 }
 
-func (c * Cache[K, V]) Get(key K) (*V, bool) {
-	val, ok := c.cache[key]
+func (c *Cache[K, V]) Get(key K) (*V, bool) {
+	entry, ok := c.cache[key]
 	if !ok {
 		return nil, ok
 	}
-	return &val, ok
+	return &entry.value, ok
+}
+
+func (c *Cache[K, V]) GetStatistics() Statistics {
+	return c.statistics
 }
--- projects/concurrency/cache_test.go
+++ projects/concurrency/cache_test.go
@@ -2,6 +2,8 @@ package concurrency
 
 import (
 	"testing"
+	"time"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,11 +31,41 @@ func TestCache(t *testing.T) {
 		require.False(t, ok)
 		require.Nil(t, val)
 
-		c.cache["name"] = "John"
+		c.Put("name", "John")
 
 		val, ok = c.Get("name")
 		require.True(t, ok)
 		require.NotNil(t, val)
 		require.Equal(t, "John", *val)
+	})
+
+	t.Run("does not exceed entryLimit", func(t *testing.T) {
+		c := NewCache[string, any](3)
+		input := map[string]any{
+			"name":   "John",
+			"age":    30,
+			"hobby":  "swimming",
+			"friend": "Amy",
+		}
+
+		for key, value := range input {
+			c.cache[key] = cacheEntry[any]{value: value, readTime: time.Time{}}
+		}
+		require.Equal(t, 3, len(c.cache))
+	})
+
+	t.Run("test hit rate", func (t *testing.T)  {
+		c := NewCache[string, any](3)
+
+		for i:=0; i < 20; i++ {
+			c.Get("name")
+		}
+		require.Equal(t, c.statistics.hitRate, 0)
+		c.Put("name", "John")
+
+		for i:=0; i < 10; i++ {
+			c.Get("name")
+		}
+		require.Equal(t, 33, c.statistics.hitRate)
 	})
 }

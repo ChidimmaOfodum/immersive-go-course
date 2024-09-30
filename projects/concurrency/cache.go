8b91ebac8b2bb8c77593cebd71a72eabcc12c4fc
Chidimma <chidimmamofodum@gmail.com>
2024-09-30 03:51:06 -0400

    using defer for unlocking

--- projects/concurrency/cache.go
+++ projects/concurrency/cache.go
@@ -21,12 +21,13 @@ type cacheEntry[V any] struct {
 
 type Cache[K comparable, V any] struct {
 	entryLimit      int
+
+	mu sync.Mutex
 	cache           map[K]cacheEntry[V]
 	unReadEvictedEntries   int
 	totalReads      int
 	totalWrites     int
 	successfulReads int
-	mu sync.Mutex
 }
 
 func NewCache[K comparable, V any](entryLimit int) Cache[K, V] {
@@ -35,13 +36,13 @@ func NewCache[K comparable, V any](entryLimit int) Cache[K, V] {
 
 func (c *Cache[K, V]) Put(key K, value V) bool {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.totalWrites += 1
 	entry := cacheEntry[V]{value: value, timeStamp: time.Now()}
 
 	if value, ok := c.cache[key]; ok {
 		entry.numberOfTimesRead = value.numberOfTimesRead
 		c.cache[key] = entry
-		c.mu.Unlock()
 		return ok
 	}
 
@@ -54,27 +55,27 @@ func (c *Cache[K, V]) Put(key K, value V) bool {
 		delete(c.cache, lastUsed)
 	}
 	c.cache[key] = entry
-	c.mu.Unlock()
 	return false
 }
 
 func (c *Cache[K, V]) Get(key K) (*V, bool) {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.totalReads += 1
 	entry, ok := c.cache[key]
 	if !ok {
-		c.mu.Unlock()
 		return nil, ok
 	}
 	entry.timeStamp = time.Now()
 	entry.numberOfTimesRead++
-	c.cache[key] = entry // ToDO 
+	c.cache[key] = entry
 	c.successfulReads += 1
-	c.mu.Unlock()
 	return &entry.value, ok
 }
 
 func (c *Cache[K, V]) GetStatistics() Statistics {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	var hitRate float64 
 	if c.totalReads != 0 {
 		result := float64(c.successfulReads)/float64(c.totalReads) * 100
